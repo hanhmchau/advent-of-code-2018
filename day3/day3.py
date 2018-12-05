c29cabab630cd3dd8877130c5d5f8d055aa4e685
M.C <hanhchaumn@gmail.com>
2018-12-05 17:10:09 +0700

    Day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -4,7 +4,8 @@ class Coords:
         self.y = y
 
 class Rectangle:
-    def __init__(self, left, top, width, height):
+    def __init__(self, id, left, top, width, height):
+        self.id = id
         self.left = left
         self.top = top
         self.width = width
@@ -31,7 +32,7 @@ def toRectangle(line):
     top = int(coords[1])
     width = int(sides[0])
     height = int(sides[1])
-    return Rectangle(left, top, width, height)
+    return Rectangle(id, left, top, width, height)
 
 size = 1000
 grid = [[ 0 for x in range(size)] for y in range(size)]
@@ -53,8 +54,25 @@ def countOverlaps(grid):
                 overlap += 1
     return overlap
 
+# PART B
 
-lines = read('day3.txt')
+def isIntact(rect, grid):
+    for i in range(rect.left, rect.left + rect.width):
+        for j in range(rect.top, rect.top + rect.height):
+            if (grid[i][j] > 1):
+                return False
+    return True
+
+def findIntactRectangle(rects, grid):
+    for rect in rects:
+        if isIntact(rect, grid):
+            return rect
+
+
+
+lines = read('day3/day3.txt')
 rectangles = toRectangles(lines)
 processRectangles(rectangles)
-print(countOverlaps(grid))
+# print(countOverlaps(grid))
+
+print(findIntactRectangle(rectangles, grid).id)
