97ba03c7684f62d8fd52f9aadac9b0860daea918
M.C <hanhchaumn@gmail.com>
2018-12-05 17:02:15 +0700

    Day 1 2018

---
+++ day1/day1a.py
@@ -0,0 +1,24 @@
+def read(file):
+    f = open(file, 'r')
+    contents = f.read()
+    offsets = contents.split()
+    return offsets
+
+def process(offsets):
+    total = 0
+    for offset in offsets:
+        total = total + int(offset)
+    return total
+
+def findRepeat(offsets):
+    sums = set()
+    i = 0
+    total = 0
+    while total not in sums:
+        sums.add(total)
+        total = total + int(offsets[i])
+        i = (i + 1) % len(offsets)
+    return total
+
+offsets = read('day1.txt')
+print(findRepeat(offsets))
